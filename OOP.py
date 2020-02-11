4da1b45fdcf6ab954f13e2afbe13cbf079d3e78f
Itumeleng091slk <59916899+Itumeleng091slk@users.noreply.github.com>
2020-02-11 10:57:51 +0200

    Update OOP.py

--- OOP.py
+++ OOP.py
@@ -1,13 +1,13 @@
 class Person:
-    def __init__(self,name,age,gender,interests):
+    def __init__(self,name,age,gender,interest=[]):
         self.name = name
-        self.age = age
+        self.age = int(age)
         self.gender = gender
-        self.interests = interests
+        self.interest = interest
 
     def hello(self):
         interest = ",".join(self.interest)
         return "hello my name is {} and I am a {} years old. My interest's are {} ".format(self.name, self.age,interest)
 
-person = Person('Ryan','30','male',"being a hardarse, agile, ssd hard drives" )  
+person = Person('Ryan','30',"male",["being a hardarse, agile, ssd hard drives"] )  
 print(person.hello())
